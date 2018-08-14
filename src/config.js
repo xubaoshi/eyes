9a03e4e5f1b88c41ba8f95fb6744def779d1b80c
xubs <xubs@jcgroup.com.cn>
2018-08-14 11:40:50 +0800

    edit code

--- src/config.js
+++ src/config.js
@@ -1,6 +1,12 @@
-export const domainName = ''
+var prod = process.env.NODE_ENV === 'production'
+export const domainName = prod ? '' : ' https://www.easy-mock.com/mock/5b719f05ebd4a208cce29bb8'
+
+// 患者接口
 export const patientApi = {
   evaluate: {
-    list: '/api/evaluate/list'  // 患者列表接口
+    list: '/api/evaluate/list' // 患者列表接口
+  },
+  banner: {
+    list: '/api/banner/list' // 首页 banner 列表
   }
 }

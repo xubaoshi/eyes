e1a00f23dc88fae5e3e9eaee071c670af4ba2590
xubs <xubs@jcgroup.com.cn>
2018-08-20 12:39:13 +0800

    edit code

--- src/store/reducers/cache.js
+++ src/store/reducers/cache.js
@@ -1,16 +1,20 @@
 import { handleActions } from 'redux-actions'
 import { SAVE } from '../types/cache'
 
-export default handleActions({
-  [SAVE] (state, action) {
-    const {key, value} = action.payload;
-    return {
-      ...state,
-      [key]: value
+export default handleActions(
+  {
+    [SAVE](state, action) {
+      const { key, value } = action.payload
+      return {
+        ...state,
+        [key]: value
+      }
     }
+  },
+  {
+    home: null, // 首页
+    evaluate: null, // 患者评价
+    consulterList: null, // 咨询者列表,
+    doctor: null
   }
-}, {
-  home: null, // 首页
-  evaluate: null, // 患者评价
-  consulterList: null // 咨询者列表
-})
+)

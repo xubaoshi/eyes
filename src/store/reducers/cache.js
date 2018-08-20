fbe8221773d603dde72884596f8f0220e7630d20
xubs <xubs@jcgroup.com.cn>
2018-08-20 11:52:32 +0800

    edit code

--- src/store/reducers/cache.js
+++ src/store/reducers/cache.js
@@ -12,10 +12,5 @@ export default handleActions({
 }, {
   home: null, // 首页
   evaluate: null, // 患者评价
-  consulterList: null, // 咨询者列表
-  doctor: {
-    evaluate: {
-      list: []
-    }
-  }
+  consulterList: null // 咨询者列表
 })

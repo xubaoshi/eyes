3ef9dde52781efe4e654f34cc8128eb378240841
xubs <xubs@jcgroup.com.cn>
2018-08-20 19:55:57 +0800

    edit code

---
+++ src/api/consult.js
@@ -0,0 +1,27 @@
+// 我的咨询
+import base from './base'
+import { patientApi } from '@/config'
+import store from '@/store/utils'
+import lang from '@/utils/lang'
+import Page from '@/utils/page'
+export default class consult extends base {
+  static async list() {
+    const url = `${this.baseUrl}${patientApi.consult.list}`
+    const arr = await this.get(url)
+    arr.forEach(item => {
+      item['dateFormat'] = lang.dateFormat(item.date, 'yyyy-MM-dd')
+    })
+    store.save('consult', arr)
+  }
+
+  static history() {
+    const url = `${this.baseUrl}${patientApi.evaluate.history}`
+    return new Page(
+      url,
+      this.processEvalData.bind(this)
+    )
+  }
+  static processEvalData(item) {
+    item['dateFormat'] = lang.dateFormat(item.date, 'yyyy-MM-dd')
+  }
+}
--- src/store/reducers/cache.js
+++ src/store/reducers/cache.js
@@ -13,9 +13,10 @@ export default handleActions(
   },
   {
     home: null, // 首页
-    evaluate: null, // 患者评价
+    evaluate: null, // 患者评价(首页)
     consulterList: null, // 咨询者列表,
     doctor: null, // 医生详情
-    doctorEvaluate: null // 医生评价列表
+    doctorEvaluate: null, // 医生评价列表(医生详情)
+    consult: null// 我的咨询列表
   }
 )
